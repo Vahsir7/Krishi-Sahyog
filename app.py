6e3113ed2b6b201ab3fb5c5c36140806766914ce
Rishav <rishavbairagya@gmail.com>
2025-04-11 19:07:30 +0530

    Updated server connections

--- app.py
+++ app.py
@@ -335,4 +335,4 @@ if __name__ == "__main__":
     # Use uvicorn.run for programmatic execution
     import uvicorn
     # reload=True is helpful for development
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
